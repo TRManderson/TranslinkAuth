bc21eed82d3129763ac96dbbdcc2830f51a0d975
Tom Manderson <TRManderson@gmail.com>
2014-08-09 15:01:31 +1000

    Commit as-is for Carl to pull

---
+++ parse.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re
+
+def _rightID(x,id):
+	try:
+		xID=x["id"]
+	except:
+		return False
+	return xID.startswith(id)
+
+def _pullDates(semstring):
+	return semstring.split("(")[1].split(";")[0].split("-")
+
+def _withinDate(startdatestr,enddatestr):
+	startdate=[int(x) for x in startdatestr.split("/")]
+	startdate=datetime(startdate[2],startdate[1],startdate[0])
+	enddate=[int(x) for x in enddatestr.split("/")]
+	enddate=datetime(enddate[2], enddate[1], enddate[0])
+	now = datetime.now()
+	if startdate < now and now < enddate:
+		return True
+	return False
+
+def _detailify(courseElement):
+	spans = courseElement.find_all('span')
+	title=[x for x in spans if _rightID(x,"DESCR$")][0].contents[0]
+	units=[x for x in spans if _rightID(x,"RESULTS$")][0].contents[0]
+	codeDiv=[x for x in courseElement.find_all('div') if _rightID(x, "win4divUQ_DRV_TERM_HTMLAREA5")][0]
+	code=re.findall("[A-Z][A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9]",str(codeDiv))[0]
+	return (code, title, units)
+
+def parsePage(page):
+	soup=BeautifulSoup(page)
+	rows=soup.find_all('div')
+	semesters=[x for x in rows if _rightID(x,"win4divACTION_NBRGP")]
+	index=[_withinDate(_pullDates(x.contents[0])[0],_pullDates(x.contents[0])[1]) for x in semesters].index(True)
+	rows=soup.find_all('tr')
+	courses=[x for x in rows if _rightID(x, "trACTION_NBR$"+str(index))]
+	return [_detailify(x) for x in courses]
---
+++ poll.py
@@ -0,0 +1,5 @@
+import mechanize
+
+rotaUserAgent="Mozilla/5.0 (Windows; U; Windows NT 5.1; es-ES; rv:1.9.2.10) Gecko/20100914 Firefox/3.6.10 (.NET CLR 3.5.30729)"
+url="https://www.sinet.uq.edu.au/psc/ps_4/EMPLOYEE/HRMS/c/UQMY_STUDENT.UQMY_CRSE_HOME.GBL"
+
