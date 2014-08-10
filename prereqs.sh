84b77ddec223ebafc2fbda837af9b3eff89d9c02
Tom Manderson <TRManderson@gmail.com>
2014-08-10 17:28:13 +1000

    Shell script to install crap

---
+++ prereqs.sh
@@ -0,0 +1,7 @@
+sudo pip install mechanize
+sudo pip install BeautifulSoup4
+sudo apt-get update
+sudo apt-get install pdftk
+sudo pip install webapp2
+sudo pip install paste
+sudo pip install WebOb
